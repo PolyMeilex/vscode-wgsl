e792426fd08b3317bb03868c954f2ae586560e5a
Poly <marynczak.bartlomiej@gmail.com>
2021-02-13 22:15:27 +0100

    Simple Fd definition provider

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,69 @@ import * as hasbin from "hasbin";
 
 import { Validator } from "./validator";
 
+class ParseIterator {
+  line: string[];
+
+  constructor(line: string[]) {
+    this.line = line;
+  }
+
+  next(): string | undefined {
+    return this.line.shift();
+  }
+}
+
+class WgslDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+  public provideDocumentSymbols(
+    document: vscode.TextDocument,
+    _token: vscode.CancellationToken
+  ): Promise<vscode.DocumentSymbol[]> {
+    return new Promise((resolve, reject) => {
+      const symbols: vscode.DocumentSymbol[] = [];
+      for (var i = 0; i < document.lineCount; i++) {
+        const line = document.lineAt(i);
+        let text = line.text.trim();
+
+        let sections = text.split(" ");
+
+        let iter = new ParseIterator(sections);
+
+        let isFn = false;
+        for (let n = iter.next(); n != undefined; n = iter.next()) {
+          if (n == "fn") {
+            isFn = true;
+            break;
+          }
+        }
+
+        if (isFn) {
+          let fn = iter.next();
+
+          if (fn) {
+            let symbol = new vscode.DocumentSymbol(
+              fn.split("(")[0],
+              "",
+              vscode.SymbolKind.Function,
+              line.range,
+              line.range
+            );
+            symbols.push(symbol);
+          }
+        }
+      }
+      resolve(symbols);
+    });
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.registerDocumentSymbolProvider(
+      { scheme: "file", language: "wgsl" },
+      new WgslDocumentSymbolProvider()
+    )
+  );
+
   hasbin.first(["cargo-wgsl"], (res) => {
     console.log(res);
     if (res) {
